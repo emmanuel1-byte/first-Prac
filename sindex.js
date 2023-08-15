835adb257fadc4029e59fd931241e7653cc79db4
HilaryEmmanuel <emmanuelhilary9@gmail.com>
2023-08-15 20:41:34 +0100

    initial commit

---
+++ sindex.js
@@ -0,0 +1,13 @@
+// setInterval(()=>document.body.style.background="green", 2000);
+// setInterval(()=>{
+//     document.body.style.background="pink";
+// }, 3000)
+
+let hideElement = function(){
+    document.getElementById('hed').style.display="none";
+}
+
+
+let showElement = function(){
+    document.getElementById('hed').style.display="block";
+}
